a4d732d4c01c8752617579836c10539b261e160a
Saomonite <981003163@qq.com>
2017-10-25 17:55:17 +0800

    3

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,6 +30,7 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
+    <transition name="fade">
     <div v-show="detailShow" class="detail">
       <div class="detail-wrapper" clearfix>
         <div class="detail-main">
@@ -37,12 +38,32 @@
           <div class="star-wrapper">
           <star :size="48" :score="seller.score"></star>
           </div>
+          <div class="title">
+          <div class="line"></div>
+          <div class="text">优惠信息</div>
+          <div class="line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li class="support-item" v-for="(item,index) in seller.supports">
+              <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+              <span class="text">{{seller.supports[index].description}}</span>
+            </li>
+          </ul>
+          <div class="title">
+          <div class="line"></div>
+          <div class="text">商家公告</div>
+          <div class="line"></div>
+          </div>
+          <div class="bulletin">
+            <div class="content">{{seller.bulletin}}</div>
+          </div>
         </div>
       </div>
-      <div class="detail-close">
+      <div class="detail-close" @click="hideDetail()">
         <i class="icon-close"></i>
       </div>
     </div>
+    </transition>
   </div>
 </template>
 
@@ -65,6 +86,9 @@ export default {
   methods: {
     showDetail() {
       this.detailShow = true
+    },
+    hideDetail() {
+      this.detailShow = false
     }
   },
   components: {
@@ -188,7 +212,13 @@ export default {
       width:100%
       height :100%
       overflow:auto
-      background:rgba(7,17,27,0.8) 
+      opacity:1
+      background:rgba(7,17,27,0.8)
+      backdrop-filter:blur(10)
+      &.fade-enter-active,&.fade-leave-active
+        transition:all 0.5s
+      &.fade-enter,&.fade-leave-active
+        opacity:0 
       .detail-wrapper
         width:100%
         min-height:100%
@@ -203,7 +233,56 @@ export default {
           .star-wrapper
             margin-top:18px
             padding:2px 0
-            text-align:center  
+            text-align:center
+          .title
+            display:flex
+            width:80%
+            margin:28px auto 24px auto
+            .line
+              flex:1
+              position:relative
+              top:-6px
+              border-bottom:1px solid rgba(255,255,255,0.2)
+            .text
+              padding:0 12px
+              font-weight:700
+              font-size:14px
+          .supports
+            width:80%
+            margin:0 auto
+            .support-item
+              padding:0 12px
+              margin-bottom:12px
+              font-size:0
+              &:last-child
+                margin-bottom:0
+              .icon
+                display:inline-block
+                width:16px
+                height:16px
+                margin-right:6px
+                background-size:16px 16px
+                background-repeat:no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height:16px
+                font-size:12px
+          .bulletin
+            width:80%
+            margin:0 auto
+            .content
+              padding:0 12px
+              line-height:24px
+              font-size:12px                        
       .detail-close
         position:relative
         width:32px
